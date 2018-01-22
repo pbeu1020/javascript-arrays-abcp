7a0ceeff9d2da408902170bf3ffb9adf647d8083
Peter Babeu <mbeenpete@hotmail.com>
2018-01-22 00:19:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 
 function removeElementFromBeginningOfArray(array) {
-  newArray = array.slice(1,2);
+  newArray = array.slice(1);
   return newArray;
 }
 
