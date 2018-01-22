aee4c69f498346880a4584122b87ee954a89c3e2
Peter Babeu <mbeenpete@hotmail.com>
2018-01-22 00:20:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,14 +24,15 @@ function accessElementInArray(array, index) {
   return array[index];
 } 
   
+function removeElementFromBeginningOfArray(array) {
+  newArray = array.slice(1);
+  return newArray;
+}
+
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
 }
 
 
-function removeElementFromBeginningOfArray(array) {
-  newArray = array.slice(1);
-  return newArray;
-}
 
